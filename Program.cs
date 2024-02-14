4defd97f658ffe517d84d0eecda9241728b54602
Dylan Moore <dylankmoore@gmail.com>
2024-02-14 15:15:35 -0600

    create campsites

--- Program.cs
+++ Program.cs
@@ -53,4 +53,12 @@ app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
     return Results.Ok(campsite);
 });
 
+//CREATE CAMPSITES
+app.MapPost("/api/campsites", (CreekRiverDbContext db, Campsite campsite) =>
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
 app.Run();
